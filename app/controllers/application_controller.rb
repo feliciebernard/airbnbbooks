75871ac4dcce56a948aa6625d30235bfd21b970f
nicolas <nicopelletier39@gmail.com>
2021-06-20 00:07:25 +0200

    Revert "devise: add cutom fields (name and biography) to devise views and modify link to update profile"
    This reverts commit 6486e5b83bcd3375bd0b20c029857a2d26379c82.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
 
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :biography, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :biography, :email, :password, :current_password)}
   end
 end
