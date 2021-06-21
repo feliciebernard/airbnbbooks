efeac852c74c5e536bf4aec450afd6f94ad39428
feliciebernard <felicie.bernard@outlook.fr>
2021-06-21 11:49:27 +0200

    add front to mails

--- spec/mailers/previews/contact_mailer_preview.rb
+++ spec/mailers/previews/contact_mailer_preview.rb
@@ -1,4 +1,13 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
+  def general_message
+
+    ContactMailer.general_message(Contact.first)
+  end
+
+  def personnal_message
+
+    ContactMailer.personnal_message(Contact.first)
+  end
 
 end
