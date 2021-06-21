fd754947534e86aede78553a782655aa11af3439
nicolas <nicopelletier39@gmail.com>
2021-06-21 08:32:43 +0200

    loan-feature: add has_many loans to user and add method to own_book

--- app/models/own_book.rb
+++ app/models/own_book.rb
@@ -4,9 +4,27 @@ class OwnBook < ApplicationRecord
 
   has_many :loans
 
-  def is_mine?
-    return self.user == current_user
+  def mine?
+    user == current_user
   end
+
+  def loan_request?
+    loans.find_by(is_accepted: false).nil? == false
+  end
+
+  def loan_request
+    loans.find_by(is_accepted: false)
+  end
+
+  def loaned?
+    loans.find_by(is_accepted: true, is_past: false).nil? == false
+  end
+  def borrowed_by
+    loans.find_by(is_accepted: true, is_past: false).borrower.name
+  end
+
+
+
  # validates :appreciation, format: { with: /[0-5]/ }, in: update
 
 
