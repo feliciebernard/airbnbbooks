479d70db3bd7d12a009729d899553fbdf8064689
feliciebernard <felicie.bernard@outlook.fr>
2021-06-23 10:57:00 +0200

    add somme translations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -89,7 +89,7 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         @own_book = @book.own_books.find_by(user: current_user)
-        format.html { redirect_to @own_book, notice: "Book was successfully updated." }
+        format.html { redirect_to @own_book, notice: "Les données du livre ont bien été mises à jour." }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
