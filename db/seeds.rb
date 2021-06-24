fc4c6565453226186084e383c03cc2861e6114a7
feliciebernard <felicie.bernard@outlook.fr>
2021-06-24 10:57:51 +0200

    add bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,13 +27,13 @@ end
 def create_books(nb_books)
   nb_books.times do |idx_book|
 
-    isbn = Faker::Number.between(from: 9790000000000, to: 9799999999999)
+    isbn = Faker::Number.between(from: 9780000000, to: 9799999999)
     title = Faker::Book.title
     authors = Faker::Book.author
     publisher = Faker::Book.publisher
     published_date = Faker::Date.between(from: 200.years.ago, to: Date.today)
     language = Faker::Nation.language
-    description = Faker::Lorem.sentences(number: rand(2..5))
+    description = Faker::Lorem.paragraph(sentence_count: rand(2..5))
     image_link = "https://res.cloudinary.com/dcmcouvju/image/upload/v1624045770/nocover2_o33zpf.jpg"
 
     book = Book.create(isbn: isbn, title: title, authors: authors, publisher: publisher, published_date: published_date, language: language, description:description, image_link:image_link)
@@ -51,7 +51,7 @@ def create_users(nb_users)
     name = Faker::Name.female_first_name
     email = Faker::Internet.email(domain: 'yopmail.com')
     password = Faker::Internet.password(min_length: 8, max_length: 15)
-    biography = Faker::Lorem.sentences(number: rand(1..3))
+    biography = Faker::Lorem.paragraph(sentence_count: rand(1..3))
     private_address = PrivateAddress.all.sample
 
     user = User.create(name: name, email: email, password: password, biography: biography, private_address: private_address)
@@ -68,7 +68,7 @@ def create_own_books(nb_own_books)
     appreciation = rand(0..5)
     user = User.all.sample
     book = Book.all.sample
-    genre = "Fiction"
+    genre = ["Fiction", "Enfant", "Historique", "Bandes-Dessinées", "Philosophie", "Loisirs"].sample
 
     own_book = OwnBook.create(review: review, appreciation: appreciation, available: true, user: user, book: book, genre: genre)
     puts "--------------- OwnBook n°#{idx_ownbook} ----------------\n\n"
@@ -82,8 +82,10 @@ def create_loans(nb_loans)
     own_book = OwnBook.all.sample
     lender = User.all.sample
     borrower = User.all.sample
+    is_past = false
+    is_accepted = false
 
-    loan = Loan.create(own_book: own_book, lender: lender, borrower: borrower)
+    loan = Loan.create(own_book: own_book, lender: lender, borrower: borrower, is_past: is_past, is_accepted: is_accepted)
     puts "--------------- Loan n°#{idx_loan} ----------------\n\n"
     status_creation(loan, 'loan', idx_loan)
 
