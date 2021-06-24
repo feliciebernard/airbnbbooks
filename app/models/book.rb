9f5a73f8f72f44ac97f232c4d6aa510d6e0963da
ltevidence <40716120+ltevidence@users.noreply.github.com>
2021-06-24 16:08:09 +0200

    Revert "creating image_link_controllers for Books"
    This reverts commit b4512078fd7f4160d437b6662037e34b04ff1276.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
   has_many :own_books
   has_many :users, through: :own_books
-  has_one_attached :image_link
   validates :title, presence: true
   validates :authors, presence: true
 end
