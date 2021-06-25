a1e96ddc255ed7ccaaf91eb7bfab2df2b5d4e007
feliciebernard <felicie.bernard@outlook.fr>
2021-06-25 08:58:18 +0200

    change the date format

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,7 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 
 }
+
+my_date_formats = { :default => '%d/%m/%Y' } 
+Time::DATE_FORMATS.merge!(my_date_formats) 
+Date::DATE_FORMATS.merge!(my_date_formats)
